1bad23ce8d0949ee989cb7fc7f35a4181ee40017
eelgarten <elgarten.e@husky.neu.edu>
2017-08-04 07:06:56 -0400

    Uploading repo

---
+++ public/javascripts/client.js
@@ -0,0 +1,100 @@
+/**
+ * Created by Elias Elgarten on 8/3/2017.
+ */
+
+var currentPrices;
+var pastPrices;
+var currencies;
+var currencyAmounts;
+
+var submitForm = function () {
+
+    var formFields = $('#inputCurrencyForm').serializeArray();
+
+    var currencyValues = formFields.filter(function (entry) {
+        return entry.value != 0;
+    });
+
+    currencies = [];
+    currencyValues.filter(function (entry) {
+        currencies.push(entry.name);
+    });
+
+    currencyAmounts = currencyValues;
+
+    getCurrentCurrencyPrices();
+};
+
+function getCurrentCurrencyPrices() {
+    return $.getJSON('/getCurrentCurrencyData/' + currencies).done(function (data) {
+        currentPrices = data;
+        console.log(currentPrices);
+
+        getPastCurrencyPrices();
+    });
+}
+
+function getPastCurrencyPrices () {
+    return $.getJSON('/getPastCurrencyData/' + currencies).done(function (data) {
+        pastPrices = data;
+        console.log(pastPrices);
+
+        calculateReturnPercentage();
+    });
+}
+
+function calculateReturnPercentage () {
+    var currentTotal = 0;
+    var pastTotal = 0;
+
+    currencyAmounts.forEach(function (item) {
+        var itemName = item.name;
+        var currentPrice = currentPrices.filter(function (currency) {
+            return currency.name == itemName;
+        });
+        currentPrice = currentPrice[0].price;
+
+        var pastPrice = pastPrices.filter(function (currency) {
+            return currency.name == itemName;
+        });
+        pastPrice = pastPrice[0].price;
+
+        var balance = item.value;
+
+        currentTotal += currentPrice * balance;
+        pastTotal += pastPrice * balance;
+
+    });
+
+    var difference = currentTotal - pastTotal;
+    var returnPercentage = difference / pastTotal;
+    
+    displayReturnPercentage(returnPercentage);
+}
+
+var displayReturnPercentage = function (returnPercentage) {
+    var displayString = "24h Portfolio Return: ";
+
+    if (returnPercentage > 0) {
+        displayString += "+";
+    }
+    else if (returnPercentage < 0) {
+        displayString += "-";
+    }
+
+    displayString += returnPercentage;
+    displayString = "<span>" + displayString + "%" + "</span>"
+
+    $('#results').html(displayString);
+}
+
+
+$(document).ready(function() {
+
+    $('#clearButton').click(function() {
+        $('#inputCurrencyForm')[0].reset();
+    });
+
+});
+
+
---
+++ routes/index.js
@@ -0,0 +1,99 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Portfolio Tracker' });
+});
+
+router.get('/getCurrentCurrencyData/:currencies', function (req, res) {
+
+  if (!req.params.currencies) {
+    res.send({
+      "error": "Request sent with no parameters."
+    });
+    console.log("Request sent with no parameters.");
+  }
+
+  var currencies = req.params.currencies;
+
+  var request = require('request');
+  var currencyData;
+
+  request('http://socket.coincap.io/front', function (err, result, body){
+    var json = JSON.parse(body);
+    var returnData = [];
+
+    currencyData = json.filter(function(currency) {
+      return currencies.includes(currency.short);
+    });
+
+    currencyData.forEach(function(curr) {
+      var obj = {};
+      obj.name = curr.short;
+      obj.price = curr.price.toString();
+
+      returnData.push(obj);
+    });
+    console.log(returnData);
+    return res.json(returnData);
+  });
+
+});
+
+router.get('/getPastCurrencyData/:currencies', function(req, res) {
+  if (!req.params.currencies) {
+    res.send({
+      "error": "Request sent with no parameters."
+    });
+    console.log("Request sent with no parameters.");
+  }
+
+  var currencies = req.params.currencies.split(',');
+  console.log(currencies);
+
+  var async = require('async');
+  var request = require('request');
+
+  function httpGet(urlObject, callback) {
+    const options = {
+      url: urlObject.url,
+      json: true
+    };
+    request (options,
+        function (err, result, body) {
+          var json = body;
+          var newObj = {};
+          newObj.name = urlObject.name;
+          newObj.price = '' + json.price.slice(-1)[0][1] + '';
+          returnData.push(newObj);
+          callback(err, body);
+        }
+    );
+  }
+
+  var urlObjects = [];
+  var returnData = [];
+
+  currencies.forEach(function (curr) {
+    var newUrl = 'http://socket.coincap.io/history/1day/' + curr;
+    var newObj = {};
+    newObj.name = curr;
+    newObj.url = newUrl;
+
+    urlObjects.push(newObj);
+  });
+
+  async.map(urlObjects, httpGet, function (err, result) {
+    if (err) return console.log(err);
+
+    // console.log(returnData);
+    return res.send(returnData);
+
+  });
+});
+
+
+
+module.exports = router;
